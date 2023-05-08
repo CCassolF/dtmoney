dc61f1e9a97690d01e553db6870835f416ed5e73
carlos <carloscassolfilho@gmail.com>
2023-05-08 20:30:00 -0300

    Listando transacoes e seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal'
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saidas.svg'
 import closeImg from '../../assets/vector.svg'
-import { api } from '../../services/api'
+import { api } from '../services/api'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
