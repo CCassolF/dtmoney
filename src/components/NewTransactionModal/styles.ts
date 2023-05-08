239145134238d9c38bdd4d6e0b71b91697771c9b
carlos <carloscassolfilho@gmail.com>
2023-05-08 18:31:21 -0300

    Cores nos botoes

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
   h2 {
@@ -58,6 +58,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean
+  activeColor: 'green' | 'red'
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D',
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -67,7 +73,7 @@ export const RadioBox = styled.button<RadioBoxProps>`
 
     background: ${(props) => 
       props.isActive 
-        ? '#CCC' 
+        ? transparentize(0.9, colors[props.activeColor])
         : 'transparent'};
 
     display: flex;
