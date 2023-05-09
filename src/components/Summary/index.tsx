2dd0d153e663c50b26fd57fc941ca8e062f3c091
carlos <carloscassolfilho@gmail.com>
2023-05-08 21:59:18 -0300

    Carregando transacoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import { Container } from "./styles";
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saidas.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext)
+
+  console.log(transactions)
+
   return(
     <Container>
       <div>
